ecd72e0f13bf92ff5aa2b92d08a963841e859c39
Valegor <medifit2000@yandex.ru>
2021-08-27 22:54:33 +0300

    first commit

---
+++ resources/js/components/Posts/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Post</th>
+                <th>Created date</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Something</td>
+                <td>Longer text</td>
+                <td>2020-01-23</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Something2</td>
+                <td>Longer text2</td>
+                <td>2020-01-22</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Something3</td>
+                <td>Longer text3</td>
+                <td>2020-03-23</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Something3</td>
+                <td>Longer text3</td>
+                <td>2020-03-23</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+
+</script>
