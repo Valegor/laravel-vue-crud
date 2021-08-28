19be32c54e1c9e1ce660f81c21b20f8fbfa09406
Valegor <medifit2000@yandex.ru>
2021-08-28 08:20:44 +0300

    install vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,17 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
 
+window.Vue = require('vue').default;
+
+
+Vue.component('posts-index', require('./components/Posts/Index').default);
+
+
+const app = new Vue({
+    el: '#app',
+});
